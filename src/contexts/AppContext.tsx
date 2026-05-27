e939028756f109278b8608559bbc6359b46b8ee0
AdrianDevid <adriandevid36@gmail.com>
2026-05-27 15:30:33 -0300

    ajuste path base

--- src/contexts/AppContext.tsx
+++ src/contexts/AppContext.tsx
@@ -138,7 +138,8 @@ export function AppProvider({ children }: { children: React.ReactNode }) {
     const storedToken = getStoredToken();
 
     const newSocket = io(`${process.env.NEXT_PUBLIC_WS_URL}/?token=${storedToken}`, {
-      transports: ["websocket", "polling"]
+      transports: ["websocket", "polling"],
+      path: "/"
     });
 
     setSocket(newSocket);
