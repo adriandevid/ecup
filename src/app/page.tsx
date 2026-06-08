6991434cd4602810df3896f148aad65c0171ef1c
adriandevid <adriandevid36@gmail.com>
2026-06-08 10:03:51 -0500

    ajuste

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,32 +6,42 @@ import { Permission } from "@/types";
 import { cookies } from "next/headers";
 
 export default async function Home() {
-  const cookieStore = await cookies();
-  const token = cookieStore.get('futchamp_token');
+  try {
+    const cookieStore = await cookies();
+    const token = cookieStore.get('futchamp_token');
 
-  if (token) {
-    const payload = await verifyToken(token.value);
-    const permission = await queryOne<Permission>(
-      'SELECT id, role_id, user_id FROM permissions WHERE user_id = $1',
-      [payload.userId]
-    );
-
-    if(permission != null) {
-      return (
-        <AppProvider roleId={permission.role_id}>
-          <div className="text-slate-100 min-h-screen flex flex-col">
-            <AppContent />
-          </div>
-        </AppProvider>
+    if (token) {
+      const payload = await verifyToken(token.value);
+      const permission = await queryOne<Permission>(
+        'SELECT id, role_id, user_id FROM permissions WHERE user_id = $1',
+        [payload.userId]
       );
-    }
-  }
 
-  return (
-    <AppProvider roleId={0}>
-      <div className="text-slate-100 min-h-screen flex flex-col">
-        <AppContent />
-      </div>
-    </AppProvider>
-  );
+      if (permission != null) {
+        return (
+          <AppProvider roleId={permission.role_id}>
+            <div className="text-slate-100 min-h-screen flex flex-col">
+              <AppContent />
+            </div>
+          </AppProvider>
+        );
+      }
+    }
+
+    return (
+      <AppProvider roleId={0}>
+        <div className="text-slate-100 min-h-screen flex flex-col">
+          <AppContent />
+        </div>
+      </AppProvider>
+    );
+  } catch (ex) {
+    return (
+      <AppProvider roleId={0}>
+        <div className="text-slate-100 min-h-screen flex flex-col">
+          <AppContent />
+        </div>
+      </AppProvider>
+    );
+  }
 }
