6708b41d10ceed129664ce35e7b9c45074aee3d9
adriandevid <adriandevid36@gmail.com>
2026-06-08 10:05:43 -0500

    ajuste

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,7 +35,7 @@ export default async function Home() {
         </div>
       </AppProvider>
     );
-  } catch (ex) {
+  } catch (ex: any) {
     return (
       <AppProvider roleId={0}>
         <div className="text-slate-100 min-h-screen flex flex-col">
